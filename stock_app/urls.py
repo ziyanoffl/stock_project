1162ff743b8c6e49becd3f017cee561fc5627be3
ziyanoffl <86348725+ziyanoffl@users.noreply.github.com>
2023-08-17 14:20:40 +0530

    Added stock suggestions feature

--- stock_app/urls.py
+++ stock_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path # Import the path function
-from .views import home_view, stock_view # Import the home_view and stock_view functions
+from .views import home_view, stock_view, find_suitable_stocks, \
+    stock_suggestion  # Import the home_view and stock_view functions
 
 urlpatterns = [
     path('', home_view, name='home_view'), # Add a url pattern for your home_view function
-    path('stock/?', stock_view, name='stock_view'), # Add a url pattern for your stock_view function
+    path('stock/', stock_view, name='stock_view'), # Add a url pattern for your stock_view function
+    path('find_suitable_stocks/', find_suitable_stocks, name='find_suitable_stocks'),
+    path('stock_suggestion/', stock_suggestion, name='stock_suggestion'),
 ]
--- stock_app/views.py
+++ stock_app/views.py
@@ -25,11 +25,17 @@ def home_view(request):  # Define a home_view function that takes a request as a
                   {'stock': stock})  # Render the home.html template and pass the stock variable as context
 
 
+def stock_suggestion(request):
+    return render(request,
+                  'suggested_stocks.html')  # Render the home.html template and pass the stock variable as context
+
+
 def stock_view(request):  # Define a stock_view function that takes a request and a stock symbol as arguments
     stock = request.GET.get('stock')
     # Get the investment amount and days from the request and convert them to float and int respectively. If they are None, set them to some default values.
     investment = request.GET.get('investment')
     days = request.GET.get('days')
+    profit_margin = request.GET.get('profit_margin')
     if investment:
         investment = float(investment)
     else:
@@ -39,6 +45,11 @@ def stock_view(request):  # Define a stock_view function that takes a request an
     else:
         days = 10  # Default number of days
 
+    if profit_margin:
+        profit_margin = int(profit_margin)
+    else:
+        profit_margin = 20
+
     # Download the stock data as a pandas dataframe using the yfinance.download function. You don't need to save it to the database.
     from datetime import datetime, timedelta
 
@@ -47,10 +58,14 @@ def stock_view(request):  # Define a stock_view function that takes a request an
 
     # Replace 'end' parameter with yesterday's date
     df = yf.download(stock, period='3y', interval='1d')
-    print(df)
+    # print(df)
+    np.random.seed(0)
 
-    # Fetching ticker values from Yahoo Finance API
-    df_ml = df[['Adj Close']]
+    # Create a copy of the DataFrame using .loc
+    df_copy = df.loc[:, :].copy()
+
+    # Select the adjusted close column as the target variable
+    df_ml = df_copy[['Adj Close']].copy()
     forecast_out = int(days)
     df_ml['Prediction'] = df_ml[['Adj Close']].shift(-forecast_out)
 
@@ -64,7 +79,12 @@ def stock_view(request):  # Define a stock_view function that takes a request an
     X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
 
     # Applying Linear Regression
-    clf = LinearRegression()
+    clf = LinearRegression(
+        copy_X=True,
+        fit_intercept=True,
+        n_jobs=None,
+        positive=False
+    )
     clf.fit(X_train, y_train)
 
     # Prediction Score
@@ -154,7 +174,115 @@ def stock_view(request):  # Define a stock_view function that takes a request an
         'historical_plot': historical_plot,
         # 'predicted_chart': 'predicted_prices.png',
         'df_latest': df_latest,
+        'profit_margin': profit_margin,
     }
 
     # Render the template with the context using the render function.
     return render(request, 'stock.html', context)
+
+
+def find_suitable_stocks(request):
+    np.random.seed(0)
+    target_profit = request.GET.get('profit_margin')  # Desired profit amount
+    time_interval = request.GET.get('time_interval')  # Time interval in days
+    investment = request.GET.get('investment')
+
+    if investment:
+        investment = float(investment)
+    else:
+        investment = 1000  # Default investment amount
+    if time_interval:
+        time_interval = int(time_interval)
+    else:
+        days = 10  # Default number of days
+
+    if target_profit:
+        target_profit = int(target_profit)
+    else:
+        target_profit = 20
+
+    # Define a list of stock symbols to consider
+    stock_symbols = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'TCEHY', 'NVDA', 'TSLA', 'BRK-B', 'META', 'TSM', 'V', 'LLY', 'UNH', 'MC.PA', 'JNJ', 'JPM', 'XOM', 'WMT', 'NVO', 'MA']
+
+    suitable_stocks = []
+
+    for stock in stock_symbols:
+        # Calculate potential profit for the specified time interval
+        potential_profit = calculate_potential_profit(stock, target_profit, time_interval, investment)
+
+        if potential_profit >= target_profit:
+            suitable_stocks.append({
+                'symbol': stock,
+                'potential_profit': potential_profit
+            })
+
+    context = {
+        'target_profit': target_profit,
+        'time_interval': time_interval,
+        'suitable_stocks': suitable_stocks,
+        'investment': investment,
+    }
+
+    return render(request, 'suggested_stocks.html', context)
+
+
+def calculate_potential_profit(stock, target_profit, time_interval, investment):
+    # Your existing code for data fetching and model training
+    # Replace 'end' parameter with yesterday's date
+    df = yf.download(stock, period='3y', interval='1d')
+    # print(df)
+
+    # Create a copy of the DataFrame using .loc
+    df_copy = df.loc[:, :].copy()
+
+    # Select the adjusted close column as the target variable
+    df_ml = df_copy[['Adj Close']].copy()
+    forecast_out = int(time_interval)
+    df_ml['Prediction'] = df_ml[['Adj Close']].shift(-forecast_out)
+
+    # Splitting data for Test and Train
+    X = np.array(df_ml.drop(['Prediction'], axis=1))
+    X = preprocessing.scale(X)
+    X_forecast = X[-forecast_out:]
+    X = X[:-forecast_out]
+    y = np.array(df_ml['Prediction'])
+    y = y[:-forecast_out]
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
+
+    # Applying Linear Regression
+    clf = LinearRegression(
+        copy_X=True,
+        fit_intercept=True,
+        n_jobs=None,
+        positive=False
+    )
+    clf.fit(X_train, y_train)
+
+    # Predicting for 'n' days stock data
+    forecast_prediction = clf.predict(X_forecast)
+    forecast = forecast_prediction.tolist()
+
+    # Get the share price when invested from yesterday's closing price which should be today's opening price
+    price_invested = df.iloc[-1]['Adj Close']
+
+    # Assuming X is a numpy array that stores the original data
+    X = np.array(df_ml['Adj Close'])
+
+    # Calculate potential profit based on the model's prediction
+    last_prediction = forecast[-1]
+    std_train = np.std(X_train)
+    mean_train = np.mean(X_train)
+    price_sold = (last_prediction * std_train) + mean_train
+
+    shares = investment / price_invested
+    potential_profit = (price_sold - price_invested) * shares
+
+    # Convert time interval to number of days
+    current_date = dt.datetime.today()
+    target_date = current_date + dt.timedelta(days=time_interval)
+
+    # Check if the potential profit is greater than or equal to the target profit
+    if potential_profit >= target_profit:
+        return potential_profit
+    else:
+        return 0
